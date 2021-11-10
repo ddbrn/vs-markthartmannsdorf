8d45235f1f8600c4842910c45ee1eec1c6e2b502
simon-sgr <simon.schoeggler@hotmail.com>
2021-11-10 10:25:06 +0100

    Classes created

---
+++ src/main/java/at/vs/vsmarkthartmannsdorf/data/SchoolClass.java
@@ -0,0 +1,4 @@
+package at.vs.vsmarkthartmannsdorf.data;
+
+public class SchoolClass {
+}
---
+++ src/main/java/at/vs/vsmarkthartmannsdorf/data/Teacher.java
@@ -0,0 +1,11 @@
+package at.vs.vsmarkthartmannsdorf.data;
+
+import java.util.List;
+
+public class Teacher {
+    private String firstname;
+    private String surname;
+    private String abbreviation;
+
+    private List<Subject> subjects;
+}
