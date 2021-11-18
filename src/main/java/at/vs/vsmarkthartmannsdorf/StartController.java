e28dbe1d7bfa6d2648dfe236cb5a8ee2976cab12
moinity <david-brannan@outlook.at>
2021-11-18 20:01:18 +0100

    Teacher

--- src/main/java/at/vs/vsmarkthartmannsdorf/StartController.java
+++ src/main/java/at/vs/vsmarkthartmannsdorf/StartController.java
@@ -1,14 +1,40 @@
 package at.vs.vsmarkthartmannsdorf;
 
+import at.vs.vsmarkthartmannsdorf.data.Teacher;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.paint.Color;
+
+import java.util.*;
 
 public class StartController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public ListView<Teacher> teacherList;
+
+    private ObservableList<Teacher> teachers = FXCollections.observableArrayList();
+
+    @FXML
+    protected void onAddTeacher(){
+        Teacher teacher = new Teacher("Simon", "Schöggler", "SS", null);
+
+        teachers.add(teacher);
+
+        teacherList.setItems(teachers);
+    }
+
+    @FXML
+    protected void onRemoveTeacher(){
+        int index = teacherList.getSelectionModel().getSelectedIndex();
+
+        teachers.remove(index);
+        teacherList.setItems(teachers);
     }
 }
--- src/main/java/at/vs/vsmarkthartmannsdorf/data/Teacher.java
+++ src/main/java/at/vs/vsmarkthartmannsdorf/data/Teacher.java
@@ -1,11 +1,22 @@
 package at.vs.vsmarkthartmannsdorf.data;
 
 import java.util.List;
-
 public class Teacher {
     private String firstname;
     private String surname;
     private String abbreviation;
 
     private List<Subject> subjects;
+
+    public Teacher(String firstname, String surname, String abbreviation, List<Subject> subjects) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.abbreviation = abbreviation;
+        this.subjects = subjects;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", surname.toUpperCase(), firstname);
+    }
 }
