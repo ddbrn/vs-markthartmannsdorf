f565897b666d4e363c2894c0fe4ec21252730d7c
simon-sgr <simon.schoeggler@hotmail.com>
2021-11-25 21:54:26 +0100

    LocalStorage

--- src/main/java/at/vs/vsmarkthartmannsdorf/bl/IOAccess.java
+++ src/main/java/at/vs/vsmarkthartmannsdorf/bl/IOAccess.java
@@ -6,37 +6,45 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-// 25.11.2021 Simon
+import at.vs.vsmarkthartmannsdorf.data.SchoolClass;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+// 25.11.2021
 public class IOAccess {
 
-    private static File FILE_STUDENT = Paths.get(System.getProperty("user.dir"), "src", "main", "resources","data.json").toFile();
+    private static File FILE_CLASS = Paths.get(System.getProperty("user.dir"), "src", "main", "resources","class.json").toFile();
 
-    public static boolean storeUserFiles(String jsonStr) {
+    public static boolean storeClassFiles(List<SchoolClass> schoolClassList) {
         try {
 
-            FileWriter fileWriter = new FileWriter(IOAccess.FILE_STUDENT.getAbsolutePath());
+            ObjectMapper om = new ObjectMapper();
+            String jsonStr = om.writeValueAsString(schoolClassList);
+
+            FileWriter fileWriter = new FileWriter(IOAccess.FILE_CLASS.getAbsolutePath());
             fileWriter.write(jsonStr);
             fileWriter.close();
-            System.out.println("FileWrite wrote in \"" + IOAccess.FILE_STUDENT.getName() + "\".");
+            System.out.println("FileWrite wrote in \"" + IOAccess.FILE_CLASS.getName() + "\".");
 
             return true; //successfully wrote data
 
         } catch (IOException e) {
-            System.out.println("Failed to write in the File: \"" + IOAccess.FILE_STUDENT.getName() + "\".");
+            System.out.println("Failed to write in the File: \"" + IOAccess.FILE_CLASS.getName() + "\".");
             e.printStackTrace();
             return false; //not successfully wrote data
         }
     }
 
-    public static String readUserFiles() {
-        String result = "";
+    public static List<SchoolClass> readClassFiles() {
+        List<SchoolClass> schoolClassList = new ArrayList<>();
         try  {
-            result = Files.readString(Paths.get(FILE_STUDENT.getAbsolutePath()));
+            String result = Files.readString(Paths.get(FILE_CLASS.getAbsolutePath()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return result;
+        return schoolClassList;
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module at.vs.vsmarkthartmannsdorf {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires com.fasterxml.jackson.databind;
 
     opens at.vs.vsmarkthartmannsdorf to javafx.fxml;
     exports at.vs.vsmarkthartmannsdorf;
