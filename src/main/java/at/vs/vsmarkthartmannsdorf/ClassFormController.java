252fb3ec6d81568d9301920a4ba6e53e85d0209f
MarcelSchmidl <smimaa18@htl-kaindorf.at>
2022-02-01 16:11:38 +0100

    Klassen für neue GUI

---
+++ src/main/java/at/vs/vsmarkthartmannsdorf/ClassFormController.java
@@ -0,0 +1,62 @@
+package at.vs.vsmarkthartmannsdorf;
+
+import at.vs.vsmarkthartmannsdorf.data.Teacher;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClassFormController implements Initializable {
+
+    @FXML
+    public TextField classname;
+    public ChoiceBox<Teacher> classTeacher;
+    public Label info;
+
+    private ObservableList<Teacher> teachers;
+
+    private ClassViewController parent;
+
+    public ClassFormController() {
+        this.teachers = FXCollections.observableArrayList();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        info.setVisible(false);
+    }
+
+    public void setParent(ClassViewController parent) {
+        this.parent = parent;
+        teachers.setAll(this.parent.getParent().getTeacher());
+    }
+    @FXML
+    public void cancelClasses(){
+        parent.dismountForm();
+    }
+
+    @FXML
+    public void submitClasses(){
+        info.setVisible(false);
+        if(!(classname.getText().isEmpty() || classTeacher.getValue() == null)){
+            if(parent.getClasses().stream().noneMatch(klasse -> klasse.getClassname().equalsIgnoreCase(classname.getText()))){
+                parent.submitForm(classname.getText(), classTeacher.getValue());
+            }else{
+                info.setText("Dieses Klasse existiert bereits!");
+                info.setVisible(true);
+            }
+        }else{
+            info.setText("Bitte alle Felder ausfüllen!");
+            info.setVisible(true);
+        }
+
+    }
+
+}
