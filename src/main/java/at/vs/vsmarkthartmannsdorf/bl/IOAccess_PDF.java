0aa56d65acaf975c1be8e1dcd78b330702b096f6
MarcelSchmidl <smimaa18@htl-kaindorf.at>
2022-03-10 16:24:50 +0100

    Farben im Stundenplan

--- src/main/java/at/vs/vsmarkthartmannsdorf/bl/IOAccess_PDF.java
+++ src/main/java/at/vs/vsmarkthartmannsdorf/bl/IOAccess_PDF.java
@@ -2,11 +2,14 @@ package at.vs.vsmarkthartmannsdorf.bl;
 
 import at.vs.vsmarkthartmannsdorf.data.PropertyName;
 import at.vs.vsmarkthartmannsdorf.data.SchoolClass;
+import at.vs.vsmarkthartmannsdorf.data.Subject;
 import at.vs.vsmarkthartmannsdorf.data.TimetableDay;
+import com.fasterxml.jackson.databind.ser.Serializers;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import javafx.scene.paint.Color;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
@@ -70,6 +73,89 @@ public class IOAccess_PDF {
             table.addCell(timetableDay.getWednesday());
             table.addCell(timetableDay.getThursday());
             table.addCell(timetableDay.getFriday());
+
+            PdfPCell[] cells = table.getRow(timetableDay.getId()).getCells();
+
+            for (PdfPCell cell : cells) {
+                Color color;
+                System.out.println(cell.getPhrase().getContent());
+
+                String hex;
+                int red, blue, green;
+                switch (cell.getPhrase().getContent()){
+                    case "Mathematik":
+                         color = Color.valueOf(PropertiesLoader.getInstance().
+                                getProperties().getProperty(Subject.Mathematik.name()));
+                         hex = color.toString().substring(2);
+                         red = Integer.parseInt(hex.substring(1, 3), 16);
+                         blue = Integer.parseInt(hex.substring(3, 5), 16);
+                         green = Integer.parseInt(hex.substring(5, 7), 16);
+
+                        cell.setBackgroundColor(new BaseColor(red, blue, green));
+                        break;
+                    case "Deutsch":
+                         color = Color.valueOf(PropertiesLoader.getInstance().
+                                getProperties().getProperty(Subject.Deutsch.name()));
+                        hex = color.toString().substring(2);
+                        red = Integer.parseInt(hex.substring(1, 3), 16);
+                        blue = Integer.parseInt(hex.substring(3, 5), 16);
+                        green = Integer.parseInt(hex.substring(5, 7), 16);
+
+                        cell.setBackgroundColor(new BaseColor(red, blue, green));
+                        break;
+                    case "Sport":
+                         color = Color.valueOf(PropertiesLoader.getInstance().
+                                getProperties().getProperty(Subject.Sport.name()));
+                        hex = color.toString().substring(2);
+                        red = Integer.parseInt(hex.substring(1, 3), 16);
+                        blue = Integer.parseInt(hex.substring(3, 5), 16);
+                        green = Integer.parseInt(hex.substring(5, 7), 16);
+
+                        cell.setBackgroundColor(new BaseColor(red, blue, green));
+                        break;
+                    case "Musik":
+                        color = Color.valueOf(PropertiesLoader.getInstance().
+                                getProperties().getProperty(Subject.Musik.name()));
+                        hex = color.toString().substring(2);
+                        red = Integer.parseInt(hex.substring(1, 3), 16);
+                        blue = Integer.parseInt(hex.substring(3, 5), 16);
+                        green = Integer.parseInt(hex.substring(5, 7), 16);
+
+                        cell.setBackgroundColor(new BaseColor(red, blue, green));
+                        break;
+                    case "Sachkunde":
+                        color = Color.valueOf(PropertiesLoader.getInstance().
+                                getProperties().getProperty(Subject.Sachkunde.name()));
+                        hex = color.toString().substring(2);
+                        red = Integer.parseInt(hex.substring(1, 3), 16);
+                        blue = Integer.parseInt(hex.substring(3, 5), 16);
+                        green = Integer.parseInt(hex.substring(5, 7), 16);
+
+                        cell.setBackgroundColor(new BaseColor(red, blue, green));
+                        break;
+                    case "Englisch":
+                        color = Color.valueOf(PropertiesLoader.getInstance().
+                                getProperties().getProperty(Subject.Englisch.name()));
+                        hex = color.toString().substring(2);
+                        red = Integer.parseInt(hex.substring(1, 3), 16);
+                        blue = Integer.parseInt(hex.substring(3, 5), 16);
+                        green = Integer.parseInt(hex.substring(5, 7), 16);
+
+                        cell.setBackgroundColor(new BaseColor(red, blue, green));
+                        break;
+                    case "Sozialkunde":
+                        color = Color.valueOf(PropertiesLoader.getInstance().
+                                getProperties().getProperty(Subject.Sozialkunde.name()));
+                        hex = color.toString().substring(2);
+                        red = Integer.parseInt(hex.substring(1, 3), 16);
+                        blue = Integer.parseInt(hex.substring(3, 5), 16);
+                        green = Integer.parseInt(hex.substring(5, 7), 16);
+
+                        cell.setBackgroundColor(new BaseColor(red, blue, green));
+                        break;
+                    default:
+                }
+            }
         }
     }
     private static void addCustomRows(PdfPTable table) {
