9db10ba98e2d3e432573954745e605832d9ae4a3
Mxnu-jpg <82145853+Mxnu-jpg@users.noreply.github.com>
2022-03-10 15:26:51 +0100

    Help

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,5 +13,4 @@ module at.vs.vsmarkthartmannsdorf {
     opens at.vs.vsmarkthartmannsdorf to javafx.fxml;
     exports at.vs.vsmarkthartmannsdorf;
     exports at.vs.vsmarkthartmannsdorf.data;
-    opens at.vs.vsmarkthartmannsdorf.old to javafx.fxml;
 }
