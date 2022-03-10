b6c06a2e8f9ce3955d25f49570d099cef5aeecc4
MarcelSchmidl <smimaa18@htl-kaindorf.at>
2022-03-10 16:25:25 +0100

    Farben im Stundenplan

--- src/main/java/at/vs/vsmarkthartmannsdorf/bl/IOAccess_PDF.java
+++ src/main/java/at/vs/vsmarkthartmannsdorf/bl/IOAccess_PDF.java
@@ -44,10 +44,10 @@ public class IOAccess_PDF {
 
         document.open();
 
+        System.out.println(schoolClass.getTimetable().getTimeTableContent().size());
         PdfPTable table = new PdfPTable(6);
         addTableHeader(table);
         addRows(table);
-        addCustomRows(table);
 
         document.add(table);
         document.close();
@@ -157,15 +157,5 @@ public class IOAccess_PDF {
                 }
             }
         }
-    }
-    private static void addCustomRows(PdfPTable table) {
-
-        PdfPCell horizontalAlignCell = new PdfPCell(new Phrase("row 2, col 2"));
-        horizontalAlignCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        table.addCell(horizontalAlignCell);
-
-        PdfPCell verticalAlignCell = new PdfPCell(new Phrase("row 2, col 3"));
-        verticalAlignCell.setVerticalAlignment(Element.ALIGN_BOTTOM);
-        table.addCell(verticalAlignCell);
     }
 }
