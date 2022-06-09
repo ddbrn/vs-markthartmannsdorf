49823142d342e48eeaa3b48d11acf202bda49c60
simon <simon.schoeggler@hotmail.com>
2022-06-09 07:46:29 +0200

    ka

--- src/main/java/at/vs/vsmarkthartmannsdorf/data/Subjectobject.java
+++ src/main/java/at/vs/vsmarkthartmannsdorf/data/Subjectobject.java
@@ -3,9 +3,11 @@ package at.vs.vsmarkthartmannsdorf.data;
 import javafx.scene.paint.Color;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Subjectobject {
     private String name;
     private Color color;
